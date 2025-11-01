6219201596f32d3d02da784717d0191b4fa07069
sadan <117494111+sadan4@users.noreply.github.com>
2025-11-01 15:15:16 -0400

    apply formatting

--- flake.nix
+++ flake.nix
@@ -1,16 +1,14 @@
 {
-  description = "A faster, persistent implementation of `direnv`'s `use_nix`, to replace the built-in one.";
+	description = "A faster, persistent implementation of `direnv`'s `use_nix`, to replace the built-in one.";
 
-  inputs = { };
+	inputs = {};
 
-  outputs =
-    { ... }:
-    {
-      templates = {
-        default = {
-          path = ./template;
-          description = "nix flake new -t github:nix-community/nix-direnv .";
-        };
-      };
-    };
+	outputs = {...}: {
+		templates = {
+			default = {
+				path = ./template;
+				description = "nix flake new -t github:nix-community/nix-direnv .";
+			};
+		};
+	};
 }
--- template/flake.nix
+++ template/flake.nix
@@ -1,36 +1,39 @@
 {
-  description = "A basic flake with a shell";
-  inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.05";
-    };
-    systems = {
-      url = "github:nix-systems/default";
-    };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
-  };
+	description = "A basic flake with a shell";
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-25.05";
+		};
+		systems = {
+			url = "github:nix-systems/default";
+		};
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+			inputs.systems.follows = "systems";
+		};
+	};
 
-  outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            hello
-          ];
-          shellHook = ''
-            #shell config here
-          '';
-        };
-      }
-    );
+	outputs = {
+		nixpkgs,
+		flake-utils,
+		...
+	}:
+		flake-utils.lib.eachDefaultSystem (
+			system: let
+				pkgs =
+					import nixpkgs {
+						inherit system;
+					};
+			in {
+				devShells.default =
+					pkgs.mkShell {
+						packages = with pkgs; [
+							hello
+						];
+						shellHook = ''
+							#shell config here
+						'';
+					};
+			}
+		);
 }
